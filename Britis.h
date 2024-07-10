8b9515b1a2af11d6896759d95673e755c3a89149
AndyLHCb <43877985+AndyLHCb@users.noreply.github.com>
2024-07-10 10:41:01 +0200

    Add British English (#1)
    * Add British English
    
    * Add example for Britis.h

---
+++ Britis.h
@@ -0,0 +1,14 @@
+#ifndef ENGLISH_BINDINGS
+#define ENGLISH_BINDINGS
+
+#ifdef FRENCH_BINDINGS
+
+#pragma message( "BILINGUAL WARNING : Loading both British and French bindings" )
+
+#endif
+
+#define synchronised synchronized
+#define mate friend
+#define constinnit constinit
+
+#endif
